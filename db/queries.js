db1f7f5ea4cf1551c5614875e6ddef4b7b015b45
Aaron <trapp.root@gmail.com>
2025-04-14 17:42:46 -0700

    get queries done, need to finish item detail, post routes

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,25 @@
 import { pool } from "./pool.js";
 
+const getAllProducts = async () => {
+  try {
+    const { rows } = await pool.query(`
+      SELECT 
+        products.id,
+        products.name,
+        products.price,
+        products.stock_quantity,
+        categories.name AS category_name
+      FROM 
+        products
+      INNER JOIN 
+        categories ON products.category_id = categories.id;`);
+    return rows;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
+};
+
 const getAllCategories = async () => {
   try {
     const { rows } = await pool.query("SELECT * FROM categories ORDER BY id");
@@ -10,4 +30,60 @@ const getAllCategories = async () => {
   }
 };
 
-export default { getAllCategories };
+const getProdcutsByCategoryId = async (categoryId) => {
+  try {
+    const { rows } = await pool.query(
+      `
+      SELECT
+        products.id,
+        products.name,
+        products.price,
+        products.stock_quantity,
+        categories.name AS category_name
+      FROM
+        products
+      INNER JOIN
+        categories ON products.category_id = categories.id
+      WHERE
+        categories.id = $1`,
+      [categoryId]
+    );
+    return rows;
+  } catch (error) {
+    console.error("Error fetching products by category ID:", error);
+    throw error;
+  }
+};
+
+const getProductByProductId = async (productId) => {
+  try {
+    const { rows } = await pool.query(
+      `
+      SELECT
+        products.id,
+        products.name,
+        products.description,
+        products.price,
+        products.stock_quantity,
+        categories.name AS category_name
+      FROM
+        products
+      INNER JOIN
+        categories ON products.category_id = categories.id
+      WHERE
+        products.id = $1`,
+      [productId]
+    );
+    return rows[0];
+  } catch (error) {
+    console.error("Error fetching product by ID:", error);
+    throw error;
+  }
+};
+
+export default {
+  getAllProducts,
+  getAllCategories,
+  getProdcutsByCategoryId,
+  getProductByProductId,
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,8 +5,25 @@ const indexRouter = Router();
 
 indexRouter.get("/", async (req, res) => {
   const categories = await db.getAllCategories();
-  return res.render("index", { categories });
+  const products = await db.getAllProducts();
+  
+  return res.render("index", { categories, products, itemDetailView: false });
 });
 
+indexRouter.get("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  const categories = await db.getAllCategories();
+  const product = await db.getProductByProductId(productId);
+
+  return res.render("index", { categories, product, itemDetailView: true });
+})
+
+indexRouter.get("/categories/:id", async (req, res) => {
+  const categoryId = req.params.id;
+  const categories = await db.getAllCategories();
+  const products = await db.getProdcutsByCategoryId(categoryId);
+
+  return res.render("index", { categories, products, itemDetailView: false });
+})
 
 export default indexRouter;
