32d8489b2f9e0adbd3fe9ba55538da44616e1c3a
Aaron <trapp.root@gmail.com>
2025-04-15 20:20:36 -0700

    added rate limiter

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from "express";
 import indexRouter from "./routes/indexRouter.js";
+import { rateLimit } from "express-rate-limit";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 25,
+  message:
+    "Too many requests from this IP, please try again after 15 minutes.",
+})
+
+app.use(limiter);
 
 app.set("view engine", "ejs");
 
